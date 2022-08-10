f8bb9f6cf5e2ba0ba3ca4525c42fa064652dbd10
SaiyanPrideKakarot <rohanbawane.89@gmail.com>
2022-08-10 13:40:11 +0530

    updated on 10 august

--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,55 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+
+let players = [
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "gopal",
+        "dob": "1/09/1995",
+        "gender": "male",
+        "city": "delhi",
+        "sports": [
+            "soccer"
+        ],
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ],
+    },
+]
+
+router.post("/players", function(req, res){
+    let newPlayer = req.body
+    players.push(newPlayer)
+    res.send( { data : players, status : true } )
+})
+
+
 module.exports = router;
+
+
+
+
+
+// {
+//     "name": "rohan",
+//     "dob": "13/4/1998",
+//     "gender": "male",
+//     "city": "raipur",
+//     "sports": ["cricket", "swimming"]
+
+// }
