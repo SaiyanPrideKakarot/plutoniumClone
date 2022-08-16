75c0eb58f8790e4a966e0a909e71bb6d65fd84b5
SaiyanPrideKakarot <rohanbawane.89@gmail.com>
2022-08-16 23:48:53 +0530

    assignment 16 aug

---
+++ src/controllers/bookController2.js
@@ -0,0 +1,54 @@
+const BookModel2 = require('../models/bookModel2')
+
+const createBook = async function(req, res){
+    let data= req.body
+    let savedData= await BookModel2.create(data)
+    res.send({msg: savedData})
+}
+
+const bookList= async function(req, res){
+    let allBooks= await BookModel2.find().select({bookName: 1, authorName: 1})
+    res.send({msg: allBooks})
+}
+
+const getBooksInYear= async function(req, res){
+    let data= req.body
+    let booksInYear= await BookModel2.find({year: data})
+    res.send({msg: booksInYear})
+}
+
+const getParticularBooks= async function(req, res){
+    let data= req.body
+    let particularBooks= await BookModel2.find({
+        $or: [{bookName: data.bookName},
+            {price: data.price},
+            {year: data.year},
+            {authorName: data.authorName},
+            {totalPages: data.totalPages},
+            {stockAvailable: data.stockAvailable}]
+        })
+    res.send({msg: particularBooks})
+}
+
+const getXINRBooks= async function(req, res){
+    let XINRBooks= await BookModel2.find({$or:[{price: 100}, {price: 200}, {price: 500}]})
+    res.send({msg: XINRBooks})
+}
+
+const getRandombooks= async function(req, res){
+    let randomBooks= await BookModel2.find({
+        $or[
+            {stockAvailable: true},
+            {totalPages: {$gt: 500}}
+        ]
+    })
+    res.send({msg: randomBooks})
+}
+
+
+module.exports.createBook= createBook
+module.exports.bookList= bookList
+module.exports.getBooksInYear= getBooksInYear
+module.exports.getParticularBooks= getParticularBooks
+module.exports.getXINRBooks= getXINRBooks
+module.exports.getRandombooks= getRandombooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
+const BookController2= require("../controllers/bookController2")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -15,5 +16,17 @@ router.get("/getUsersData", UserController.getUsersData)
 router.post("/createBook", BookController.createBook  )
 
 router.get("/getBooksData", BookController.getBooksData)
+
+router.post("/createBook", BookController2.createBook)
+
+router.get("/bookList", BookController2.bookList)
+
+router.post("/getBooksInYear", BookController2.getBooksInYear)
+
+router.post("/getParticularBooks", BookController2.getParticularBooks)
+
+router.post("/getXINRBooks", BookController2.getXINRBooks)
+
+router.post("/getRandomBooks", BookController2.getRandombooks)
 
 module.exports = router;
