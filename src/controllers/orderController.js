66703f4361fad18035c1d850e3d8bcdc04dc9173
SaiyanPrideKakarot <rohanbawane.89@gmail.com>
2022-08-26 21:10:47 +0530

    assignment done

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,13 +1,34 @@
 const { count } = require("console")
 const orderModel = require("../models/orderModel")
+const userModel = require("../models/userModel")
 
 const createOrder = async function (req, res) {
     let newOrder = req.body
-    if (!newOrder.userId && !newOrder.productId) {
-        res.send({ msg: "Not enough information present" })
-    } else {
+    let userId= newOrder.userId
+    let productId= newOrder.productId
+    if (!userId) {
+        return res.send({ msg: "Enter valid User id" })
+    } else if(!productId){
+        return res.send({ msg: "Enter valid Product id" })
+    } else {}
+    let appUser= req.headers.isfreeappuser
+    let amountZero= 0
+    if(appUser === true){
+        newOrder.amount= amountZero
+         newOrder.isFreeAppUser= appUser
+         let savedOrder = await orderModel.create(newOrder)
+         res.send({ msg: savedOrder })
+    } else if(userId.balance >= productId.price){
+        await userModel.findOneAndUpdate(
+            {_id: userId},
+            {$set: {balance: userId.balance - productId.price}}
+            )
+        newOrder.amount= productId.price
+        newOrder.isFreeAppUser= appUser
         let savedOrder = await orderModel.create(newOrder)
         res.send({ msg: savedOrder })
+    } else {
+        res.send({ msg: "Insufficient Balance"})
     }
 }
 
